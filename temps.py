e530c140b4308b49603a370aa66c13b035aa7f41
dawsonkeyes <supermememan1337@gmail.com>
2026-03-30 15:48:52 -0600

    adjust link to use just bld02 station + temp arrays

---
+++ temps.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://coagmet.colostate.edu/data/nw/daily/bld02.json?from=start&to=now&fields=tMax"
+
+data = requests.get(url).json()
+
+dates = []
+temps = []
+
+for d, t in zip(data["time"], data["tMax"]):
+    dates.append(d)
+    temps.append(t)
+
+print(dates)
+print(temps)
