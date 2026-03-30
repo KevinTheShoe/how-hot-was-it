09d124b5d36c99aed989645dba6982196c3e02cb
Kevin <kevinshu.colorado@gmail.com>
2026-03-30 14:58:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import requests
+import json
+
+def graphTemps():
+	
 
 if __name__ == '__main__':
-	print(requests.get('https://coagmet.colostate.edu/data/daily.json?from=1991-01-01&to=now&fields=tMax').text)
+	data = json.loads(requests.get('https://coagmet.colostate.edu/data/daily.json?from=1991-01-01&to=now&fields=tMax').text)
+	print(data)
