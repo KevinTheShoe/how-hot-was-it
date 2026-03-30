9bc2e4a24955219e00e15729f80087f8843802f0
Kevin <kevinshu.colorado@gmail.com>
2026-03-30 15:07:16 -0600

    Graphing function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 import json
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def graphTemps():
-	
+def graph(x, y):
+	sns.set_theme()
+	sns.scatterplot(x=x, y=y)
+	plt.show()
 
 if __name__ == '__main__':
 	data = json.loads(requests.get('https://coagmet.colostate.edu/data/daily.json?from=1991-01-01&to=now&fields=tMax').text)
