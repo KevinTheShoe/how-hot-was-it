2814897029e517f3613383cb0164e4cf5e35ff18
dawsonkeyes <supermememan1337@gmail.com>
2026-04-06 15:01:21 -0600

    average the high by month

--- temps.py
+++ temps.py
@@ -2,15 +2,21 @@ import requests
 import pandas as pd
 
 def getAsDF():
-	url = "https://coagmet.colostate.edu/data/nw/daily/bld02.json?from=start&to=now&fields=tMax"
+    url = "https://coagmet.colostate.edu/data/nw/daily/bld02.json?from=start&to=now&fields=tMax"
 
-	data = requests.get(url).json()
+    data = requests.get(url).json()
 
-	dates = []
-	temps = []
+    dates = []
+    temps = []
 
-	for d, t in zip(data["time"], data["tMax"]):
-		dates.append(d)
-		temps.append(t)
+    for d, t in zip(data["time"], data["tMax"]):
+        dates.append(d[:7])
+        temps.append(t)
 
-	return pd.DataFrame({'time': dates, 'temp': temps})
+    df = pd.DataFrame({'time': dates, 'temp': temps})
+    df = df.groupby('time', as_index=False)['temp'].mean()
+
+    return df
+
+df = getAsDF()
+print(df)
