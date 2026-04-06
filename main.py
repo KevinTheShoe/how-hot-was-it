53ac959f7bf6c53192ba9af35ce4a80416491985
Kevin <kevinshu.colorado@gmail.com>
2026-04-06 14:50:12 -0600

    Filtering -999

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import temps
 if __name__ == '__main__':
 	df = temps.getAsDF()
 	df_filtered = df[df['time'].str.endswith('-04-05')]
+	df_filtered = df_filtered[df_filtered['temp'] != -999]
 	print(df_filtered)
 	graphing.graphDates(df_filtered['time'], df_filtered['temp'])
