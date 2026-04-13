88013fce2367e1bd83b56e55ec815c7f749de6f6
dawsonkeyes <supermememan1337@gmail.com>
2026-04-13 15:06:08 -0600

    double quotes the files

--- temps.py
+++ temps.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def getData(fields=['tAvg','tMin','tMax']):
-	url = f'https://coagmet.colostate.edu/data/nw/daily/bld01.json?dateFmt=iso&from=start&to=now&fields={','.join(fields)}'
+	url = f"https://coagmet.colostate.edu/data/nw/daily/bld01.json?dateFmt=iso&from=start&to=now&fields={','.join(fields)}"
 	
 	# pull data
 	data = pd.read_json(url)
@@ -19,7 +19,7 @@ def getData(fields=['tAvg','tMin','tMax']):
 	return data
 
 def getDataCSV(fields=['tAvg','tMin','tMax']):
-	url = f'https://coagmet.colostate.edu/data/nw/daily/bld01.csv?dateFmt=iso&from=start&to=now&fields={','.join(fields)}'
+	url = f"https://coagmet.colostate.edu/data/nw/daily/bld01.csv?dateFmt=iso&from=start&to=now&fields={','.join(fields)}"
 	
 	# pull data
 	data = pd.read_csv(url, header=None, names=['station id', 'time'] + fields)
