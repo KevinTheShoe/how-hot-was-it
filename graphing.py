d003e6d4ad8cc4ad7cfb207db0f67ff2b351d8d1
Kevin <kevinshu.colorado@gmail.com>
2026-04-12 20:57:58 -0600

    More Visualizations

--- graphing.py
+++ graphing.py
@@ -1,9 +1,51 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-def graphDates(x, y):
-	fig, ax = plt.subplots(figsize = (12,6))
-	fig = sns.barplot(x=x, y=y, estimator=sum, ax=ax)
-	x_dates = sorted(list(set(x)))
+def monthOverlaid(df, month):
+	sns.set_theme()
+	fig, ax = plt.subplots(figsize=(16, 9))
+
+	for year in range(2003, 2027):
+		new_df = df[df['time'].str.startswith(f'{year}-{month}')]
+		fig = sns.lineplot(x=new_df['time'].str[8:], y=new_df['temp'], ax=ax, label=year, linewidth=(1 if year != 2026 else 3), alpha=(0.5 if year != 2026 else 1))
+
+	ax.set(title='Month of March Daily High Temps Overlaid', xlabel='Day of March', ylabel='High Temp (Fahrenheit)')
+	ax.margins(x=0)
+	ax.set(ylim=(0, 100))
+
+	plt.tight_layout()
+	plt.show()
+
+def monthBoxplot(df, month):
+	sns.set_theme()
+	fig, ax = plt.subplots(figsize=(16, 9))
+
+	new_df = pd.DataFrame()
+	new_df['time'] = df['time'].str[:7]
+	new_df['temp'] = df['temp']
+	# new_df = new_df.groupby('time', as_index=False)['temp'].max()
+	new_df = new_df[new_df['time'].str.endswith(f'-{month}')]
+	fig = sns.boxplot(data=new_df, x='time', y='temp')
+
+	x_dates = sorted(list(set(new_df['time'].str[:4])))
+	ax.set_xticklabels(labels=x_dates, ha='right')
+	ax.set(title='Month of March High Temp Distributions Per Year', xlabel='Year', ylabel='High Temp (Fahrenheit)')
+
+	plt.tight_layout()
+	plt.show()
+
+def dayLineplot(df, day):
+	sns.set_theme()
+	fig, ax = plt.subplots(figsize=(16, 9))
+
+	new_df = df[df['time'].str.endswith(day)]
+	fig = sns.lineplot(x=new_df['time'], y=new_df['temp'], ax=ax)
+
+	x_dates = sorted(list(set(new_df['time'])))
 	ax.set_xticklabels(labels=x_dates, rotation=45, ha='right')
+	ax.set(title=f'High Temp For {day} of Each Year', xlabel='Day', ylabel='High Temp (Fahrenheit)')
+	ax.set(ylim=(0, 100))
+
+	plt.tight_layout()
 	plt.show()
--- main.py
+++ main.py
@@ -3,22 +3,11 @@ import temps
 import pandas as pd
 
 def clean(df):
-	return df[df['temp'] != -999]
-
-def averageByMonth(df):
-	new_df = pd.DataFrame()
-	new_df['time'] = df['time'].str[:7]
-	new_df['temp'] = df['temp']
-	new_df = new_df.groupby('time', as_index=False)['temp'].mean()
-	print(new_df)
-	graphing.graphDates(new_df['time'], new_df['temp'])
-
-def maxOfDay(df, day):
-	new_df = df[df['time'].str.endswith(day)]
-	graphing.graphDates(new_df['time'], new_df['temp'])
+	return df[df['temp'] > -50]
 
 if __name__ == '__main__':
 	df = temps.getJsonAsDF()
 	df = clean(df)
-	maxOfDay(df, '-04-05')
-	averageByMonth(df)
+	graphing.monthOverlaid(df, '03')
+	graphing.monthBoxplot(df, '03')
+	graphing.dayLineplot(df, '03-19')
