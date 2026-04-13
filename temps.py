a84acdf539f60a39fb89be7c97f6d8b23b25d555
Kevin <kevinshu.colorado@gmail.com>
2026-04-13 14:42:07 -0600

    Make pulling CSV data work

--- temps.py
+++ temps.py
@@ -2,10 +2,13 @@ import requests
 import pandas as pd
 
 def getData(fields=['tAvg','tMin','tMax']):
-	url = f'https://coagmet.colostate.edu/data/nw/daily/bld02.json?from=start&to=now&fields={','.join(fields)}'
+	url = f'https://coagmet.colostate.edu/data/nw/daily/bld02.json?dateFmt=iso&from=start&to=now&fields={','.join(fields)}'
 	
+	# pull data
+	data = pd.read_json(url)
+
 	# limit to desired fields
-	data = pd.read_json(url)[['time'] + fields]
+	data = data[['time'] + fields]
 
 	# clean
 	if 'tAvg' in fields: data = data[data['tAvg'] > -50]
@@ -16,18 +19,20 @@ def getData(fields=['tAvg','tMin','tMax']):
 
 	return data
 
-def getDataCSV():
-	url = "https://coagmet.colostate.edu/data/nw/daily/bld02.csv?from=start&to=now&fields=tMax"
+def getDataCSV(fields=['tAvg','tMin','tMax']):
+	url = f'https://coagmet.colostate.edu/data/nw/daily/bld02.csv?dateFmt=iso&from=start&to=now&fields={','.join(fields)}'
+	
+	# pull data
+	data = pd.read_csv(url, header=None, names=['station id', 'time'] + fields)
+	
+	# limit to desired fields
+	data = data[['time'] + fields]
 
-	data = requests.get(url).text
+	# clean
+	if 'tAvg' in fields: data = data[data['tAvg'] > -50]
+	if 'tMin' in fields: data = data[data['tMin'] > -50]
+	if 'tMax' in fields: data = data[data['tMax'] > -50]
 
-	dates = []
-	temps = []
+	print(data)
 
-	for line in data.split('\n'):
-		split = line.strip().split(',')
-		if len(split) > 2:
-			dates.append(split[1].strip("\""))
-			temps.append(float(split[2]))
-
-	return pd.DataFrame({'time': dates, 'temp': temps})
+	return data
